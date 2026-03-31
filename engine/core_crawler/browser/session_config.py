85148d538fab44574765c8a7a59fe52658151c25
Yevhen Yezerskyy <yevhen.yezerskyy@gmail.com>
2026-03-31 16:11:23 +0000

    Tune 11880 crawler pacing and active windows

--- engine/core_crawler/browser/session_config.py
+++ engine/core_crawler/browser/session_config.py
@@ -57,13 +57,13 @@ class SiteSessionConfig:
 
 LOG_FOLDER = "crawler"
 BROKER_QUEUE_MAX = 256
-ONE_ONE_EIGHTY_WINDOW_MIN_SEC = 15 * 60
-ONE_ONE_EIGHTY_WINDOW_MAX_SEC = 20 * 60
+ONE_ONE_EIGHTY_WINDOW_MIN_SEC = 10 * 60
+ONE_ONE_EIGHTY_WINDOW_MAX_SEC = 15 * 60
 ONE_ONE_EIGHTY_WINDOW_COOLDOWN_MIN_SEC = 50 * 60
 ONE_ONE_EIGHTY_WINDOW_COOLDOWN_MAX_SEC = 60 * 60
 ONE_ONE_EIGHTY_WINDOW_MAIN_REQUEST_LIMIT = 300
-ONE_ONE_EIGHTY_ACTIVE_TUNNEL_RATIO = 0.55
-ONE_ONE_EIGHTY_ACTIVE_TUNNEL_MAX = 7
+ONE_ONE_EIGHTY_ACTIVE_TUNNEL_RATIO = 0.65
+ONE_ONE_EIGHTY_ACTIVE_TUNNEL_MAX = 9
 ONE_ONE_EIGHTY_MISMATCH_VISIT_MAX = 2
 ONE_ONE_EIGHTY_MISMATCH_VISIT_PROBABILITY = 0.35
 
@@ -230,8 +230,8 @@ SITE_CONFIGS: dict[str, SiteSessionConfig] = {
         max_session_age_sec=1200,
         runtime_recycle_min_sec=300,
         runtime_recycle_max_sec=600,
-        pause_min_sec=4.0,
-        pause_max_sec=9.0,
+        pause_min_sec=6.0,
+        pause_max_sec=12.0,
         browser_timeout_ms=90_000,
     ),
     "gs": SiteSessionConfig(
